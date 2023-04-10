e638e010900671d165f74aa835c42eaf13084985
Margot Paon Garcia <margot.paon@gmail.com>
2023-04-10 17:26:53 -0300

    Controle de fluxo

---
+++ basico/controle-fluxo/CondicionalAula01.java
@@ -0,0 +1,11 @@
+public class CondicionalAula01{
+    public static void main(String[] args){
+      bool isTrue = false;
+	if(isTrue) {
+	  System.out.println("Dentro do if");
+	}else{
+	  System.out.println("Caiu dentro do else");
+        }
+	System.out.println("Fora do if-else");
+    }
+}
---
+++ basico/controle-fluxo/CondicionalAula02.java
@@ -0,0 +1,9 @@
+/* Receba uma idade com entrada
+   Se maior ou igual a 18 é adulto
+   Se não ainda não é adulto*/
+
+public class CondicionalAula02{
+  public static void main(String[] args){
+    // Operadores lógicos ==, !=, <, <=, >, >=
+  }
+}
