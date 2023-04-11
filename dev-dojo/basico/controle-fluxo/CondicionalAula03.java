0e27bae69e7358f0ed8335c1f89e43d62dbc38ce
Margot Paon Garcia <margot.paon@gmail.com>
2023-04-11 19:16:33 -0300

    directory refactor

--- dev-dojo/basico/controle-fluxo/CondicionalAula03.java
+++ dev-dojo/basico/controle-fluxo/CondicionalAula03.java
@@ -1,3 +1,20 @@
 /* Dado um determinado salário
    Se o mesmo for maior 4500 imprima 30% do valor
    Se não imprima 15% */
+
+import java.util.Scanner;
+
+public class CondicionalAula03{
+    public static void main(String[] args){
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Qual é seu salário?");
+	float salario =  scanner.nextFloat();
+	if(salario>=4500){
+	    float calc = salario * 0.30F;
+	    System.out.println(" O desconto sobre o salário vai ser de R$: "+calc);
+	}else{
+	    float calc = salario * 0.15F;
+	    System.out.println("O desconto sobre o salário vai ser de R$: "+calc);
+	}
+    }
+}
