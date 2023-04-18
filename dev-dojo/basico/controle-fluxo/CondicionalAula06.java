e54ae209d5314affe96ff902bb7c6cc3c2986e58
Margot Paon Garcia <margot.paon@gmail.com>
2023-04-18 11:52:55 -0300

    Novos exercicios

---
+++ dev-dojo/basico/controle-fluxo/CondicionalAula06.java
@@ -0,0 +1,16 @@
+/*
+Eu como usuário gostaria de ter o nome e a idade de participantes de um torneio de natação
+e que o sistema classifica-se as categorias das seguintes formas:
+- Até 10 anos => Nome participara da categoria infantil
+- Entre 11 e 15 anos => Nome participara da categoria juvenil
+- Entre 16 e 19 anos => Nome participara da categoria pré-adulto
+- Igual ou acima de 20 anos => Nome participara da categoria adulto
+*/
+
+import java.util.Scanner;
+
+public class CondicionalAula06{
+    public static void main(String[] args){
+	Scanner scanner = new Scanner(System.in);
+    }
+}
---
+++ dev-dojo/basico/controle-fluxo/CondicionalAula07.java
@@ -0,0 +1,11 @@
+/*
+
+Como secretário de alistamento militar necessito de um sistema que possua
+os paramentros de sexo e idade, sendo sexo M ou F e realize a seguinte
+classificação:
+
+- Se sexo for M e idade é igual ou maior que 18 anos exiba em tela alistamento obrigatório
+- Se sexo for F e idade é igual ou maior que 18 anos exiba se gostaria de se alistar
+- Se sexo for M ou F e a idade for infeiro a 18 anos exiba que o alistamento não é permito 
+
+*/
